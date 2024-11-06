92b6822c7597c0819114ad580a5ef9d0ced2b0ac
Cookiegigi <gcreusot@pm.me>
2024-11-06 18:16:58 +0800

    Implement psql complete

--- currency-conversion/src/storage/common.rs
+++ currency-conversion/src/storage/common.rs
@@ -1,10 +1,11 @@
-use std::{future::Future, time::Duration};
+use std::{future::Future, time::Duration, u64};
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 
 use super::{psql::PSQLStorageSettings, tsv::TSVStorageSettings};
+use sqlx::{postgres::{types::PgInterval, PgRow}, FromRow, Row};
 
 /// Storage type available
 #[derive(Debug, Deserialize, Serialize, Eq, PartialEq, Clone)]
@@ -26,6 +27,25 @@ pub struct DataInfoSuccess {
     pub number_of_line: usize,
 }
 
+impl sqlx::FromRow<'_, sqlx::postgres::PgRow> for DataInfoSuccess {
+    fn from_row(row: &sqlx::postgres::PgRow) -> sqlx::Result<Self> {
+        let seconds_since_last_update: PgInterval = row.try_get("seconds_since_last_update")?;
+        tracing::debug!("{:?}", seconds_since_last_update);
+        let seconds_since_last_update = Duration::new(
+            (seconds_since_last_update.months * 2678400 + seconds_since_last_update.days * 86400)
+                as u64,
+            seconds_since_last_update.microseconds as u32,
+        );
+        let number_of_line: i64 = row.try_get("number_of_line")?;
+        // Impossible to have a bad value
+        let number_of_line: usize = number_of_line.try_into().unwrap();
+        Ok(Self {
+            seconds_since_last_update,
+            number_of_line,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct DataInfoError {
     pub error: anyhow::Error,
@@ -37,11 +57,11 @@ where
     T: Serialize + for<'de> Deserialize<'de>,
 {
     /// Update all data in storage
-    fn update(&self, data: &[T]) -> impl Future<Output =  Result<()>>;
+    fn update(&self, data: &[T]) -> impl Future<Output = Result<()>>;
 
     /// Get all data from storage
-    fn get_all(&self) -> impl Future<Output =  Result<Vec<T>>>;
+    fn get_all(&self) -> impl Future<Output = Result<Vec<T>>>;
 
     /// Get informations about data (last update, number, ...)
-    fn get_data_info(&self) -> impl Future<Output =  Result<DataInfo>>;
+    fn get_data_info(&self) -> impl Future<Output = Result<DataInfo>>;
 }
--- currency-conversion/src/storage/psql.rs
+++ currency-conversion/src/storage/psql.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
-use sqlx::{postgres::PgPoolOptions, query, Acquire, PgPool, Postgres, Transaction};
+use sqlx::{postgres::PgPoolOptions, query, query_as, Acquire, PgPool, Postgres, Transaction};
 
 use crate::common::{conversion_rate::ConversionRate, supported_symbols::Symbols};
 
@@ -16,28 +16,33 @@ pub struct PSQLStorageSettings {
 }
 
 impl PSQLStorageSettings {
-
-    pub fn get_url(&self) -> Result<String>{
-        Ok(format!("postgres://{}:{}@{}:{}/{}", self.username, self.password, self.host, self.port, self.database_name))
+    pub fn get_url(&self) -> Result<String> {
+        Ok(format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        ))
     }
 }
 
 pub struct PSQLStorageManager {
     settings: PSQLStorageSettings,
-    pool: PgPool, 
+    pool: PgPool,
 }
 
 impl PSQLStorageManager {
     pub async fn from_settings(settings: PSQLStorageSettings) -> Result<PSQLStorageManager> {
-
         let url = settings.get_url()?;
         let pool = PgPoolOptions::new().connect(&url).await?;
         Ok(PSQLStorageManager { settings, pool })
     }
 
-    async fn insert_one_symbol(&self, data: &Symbols, tx:&mut Transaction<'static, Postgres>) -> Result<()>{
-
-        query!(r#"Insert Into symbols
+    async fn insert_one_symbol(
+        &self,
+        data: &Symbols,
+        tx: &mut Transaction<'static, Postgres>,
+    ) -> Result<()> {
+        query!(
+            r#"Insert Into symbols
                 (id, "name", code)
                 Values
                 (
@@ -45,14 +50,21 @@ impl PSQLStorageManager {
                 $1,
                 $2
                 )"#,
-                data.name,
-                data.code).execute(&mut **tx).await?;
+            data.name,
+            data.code
+        )
+        .execute(&mut **tx)
+        .await?;
         Ok(())
     }
 
-    async fn insert_one_conversion_rate(&self, data: &ConversionRate, tx:&mut Transaction<'static, Postgres>) -> Result<()>{
-
-        query!(r#"Insert Into conversions_rates
+    async fn insert_one_conversion_rate(
+        &self,
+        data: &ConversionRate,
+        tx: &mut Transaction<'static, Postgres>,
+    ) -> Result<()> {
+        query!(
+            r#"Insert Into conversions_rates
                 (id, "from", "to", rate)
                 Values
                 (
@@ -62,14 +74,37 @@ impl PSQLStorageManager {
 				$3
 				)
                 "#,
-                data.from,
-                data.to,
-                data.rate
-                ).execute(&mut **tx).await?;
+            data.from,
+            data.to,
+            data.rate
+        )
+        .execute(&mut **tx)
+        .await?;
         Ok(())
     }
-}
 
+    async fn update_data_info(
+        &self,
+        data_name: &str,
+        tx: &mut Transaction<'static, Postgres>,
+    ) -> Result<()> {
+        query!(
+            r#"INSERT INTO data_info 
+                (data_name, last_update_date)
+                Values
+                ($1, NOW())
+                ON CONFLICT (data_name)
+                DO 
+                UPDATE Set last_update_date = NOW();"#,
+            data_name
+        )
+        .execute(&mut **tx)
+        .await?;
+        Ok(())
+    }
+
+
+}
 
 impl StorageManager<Symbols> for PSQLStorageManager {
     async fn update(&self, data: &[Symbols]) -> Result<()> {
@@ -82,6 +117,8 @@ impl StorageManager<Symbols> for PSQLStorageManager {
             self.insert_one_symbol(item, &mut tx).await?;
         }
 
+        self.update_data_info("symbols", &mut tx).await?;
+
         tx.commit().await?;
         Ok(())
     }
@@ -89,15 +126,24 @@ impl StorageManager<Symbols> for PSQLStorageManager {
     async fn get_all(&self) -> Result<Vec<Symbols>> {
         let mut tx = self.pool.begin().await?;
 
-        let res: Vec<Symbols> = sqlx::query_as::<_, Symbols>(r#"Select code, "name" from symbols"#).fetch_all(&mut *tx).await?;
-
+        let res: Vec<Symbols> = sqlx::query_as::<_, Symbols>(r#"Select code, "name" from symbols"#)
+            .fetch_all(&mut *tx)
+            .await?;
 
         Ok(res)
     }
 
     async fn get_data_info(&self) -> Result<super::common::DataInfo> {
-        Ok(super::common::DataInfo::Error(super::common::DataInfoError { error: anyhow!("Not implemented") }))
+        let mut tx = self.pool.begin().await?;
 
+        Ok(super::common::DataInfo::Success(
+            query_as(
+                r#"Select NOW() - last_update_date as seconds_since_last_update, (Select count(*) From symbols) as number_of_line  from data_info where data_name = 'symbols'"#,
+                
+            )
+            .fetch_one(&mut *tx)
+            .await?,
+        ))
     }
 }
 
@@ -106,11 +152,15 @@ impl StorageManager<ConversionRate> for PSQLStorageManager {
         let mut tx = self.pool.begin().await?;
 
         // Delete all from symbols
-        sqlx::query("Delete from conversions_rates").execute(&mut *tx).await?;
+        sqlx::query("Delete from conversions_rates")
+            .execute(&mut *tx)
+            .await?;
 
         for item in data {
             self.insert_one_conversion_rate(item, &mut tx).await?;
         }
+
+        self.update_data_info("conversions_rates", &mut tx).await?;
 
         tx.commit().await?;
         Ok(())
@@ -119,14 +169,25 @@ impl StorageManager<ConversionRate> for PSQLStorageManager {
     async fn get_all(&self) -> Result<Vec<ConversionRate>> {
         let mut tx = self.pool.begin().await?;
 
-        let res: Vec<ConversionRate> = sqlx::query_as::<_, ConversionRate>(r#"Select "from", "to", rate from conversions_rates"#).fetch_all(&mut *tx).await?;
-
+        let res: Vec<ConversionRate> = sqlx::query_as::<_, ConversionRate>(
+            r#"Select "from", "to", rate from conversions_rates"#,
+        )
+        .fetch_all(&mut *tx)
+        .await?;
 
         Ok(res)
     }
 
     async fn get_data_info(&self) -> Result<super::common::DataInfo> {
-        Ok(super::common::DataInfo::Error(super::common::DataInfoError { error: anyhow!("Not implemented") }))
+        let mut tx = self.pool.begin().await?;
 
+        Ok(super::common::DataInfo::Success(
+            query_as(
+                r#"Select NOW() - last_update_date as seconds_since_last_update, (Select count(*) From conversions_rates) as number_of_line  from data_info where data_name = 'conversions_rates'"#,
+                
+            )
+            .fetch_one(&mut *tx)
+            .await?,
+        ))
     }
 }
